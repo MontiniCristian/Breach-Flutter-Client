c67a341c44b3b0c7646e472cfc46131168033fe9
Cristian <montinicristian@gmail.com>
2018-09-02 11:14:28 +0200

    Add a darker gray

--- lib/tools/BreachColors.dart
+++ lib/tools/BreachColors.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 /* This is the primary color */
 var grayPrimary = const Color(0xFF37464f);
 var grayLight = const Color(0xFF62717b);
-var grayDark = const Color(0xFF101f27);
 
-var marineGreen = const Color(0xFF59e5b4);
+// #101f27FF
+var grayDark = const Color(0xFF101f27);
+var grayDarker = const Color(0xFF0f1e26);
+var marineGreen = const Color(0xFF52e5b4);
